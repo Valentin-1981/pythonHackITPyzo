4273f7110c5622710f7bb2a820aa173d1587b528
Valentin-1981 <chibinyov@rambler.ru>
2023-10-31 19:45:38 +0300

    new commit

--- network_scanner.py
+++ network_scanner.py
@@ -2,14 +2,16 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    arp_request.show()
+    # arp_request.show()
     # print(arp_request.summary())
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    broadcast.show()
+    # broadcast.show()
     # scapy.ls(scapy.Ether())
     # print(broadcast.summary())
     arp_request_broadcast = broadcast/arp_request
     # print(arp_request_broadcast.summary())
-    arp_request_broadcast.show()
+    # arp_request_broadcast.show()
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
 
 scan("192.168.0.1/24")
