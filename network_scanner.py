231c52f070b4d4d5aa47c06e4c4b635f4b991cbb
Valentin-1981 <chibinyov@rambler.ru>
2023-10-31 19:32:23 +0300

    new

--- network_scanner.py
+++ network_scanner.py
@@ -2,6 +2,14 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
+    arp_request.show()
+    # print(arp_request.summary())
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast.show()
+    # scapy.ls(scapy.Ether())
+    # print(broadcast.summary())
+    arp_request_broadcast = broadcast/arp_request
+    # print(arp_request_broadcast.summary())
+    arp_request_broadcast.show()
 
 scan("192.168.0.1/24")
