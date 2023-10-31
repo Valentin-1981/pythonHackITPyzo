06fb68480951b8092930dae202c394c41c554e0d
Valentin-1981 <chibinyov@rambler.ru>
2023-10-31 21:59:26 +0300

    another commit

--- network_scanner.py
+++ network_scanner.py
@@ -11,12 +11,11 @@ def scan(ip):
     arp_request_broadcast = broadcast/arp_request
     # print(arp_request_broadcast.summary())
     # arp_request_broadcast.show()
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     # print(answered_list.summary())
+    print("IP\t\t\t\tMAC Address\n---------------------------------------------------------")
     for element in answered_list:
         # print(element[1].show())
-        print(element[1].psrc)
-        print(element[1].hwsrc)
-        print("----------------------------------------------------------")
+        print(element[1].psrc + "\t\t" + element[1].hwsrc)
 
 scan("192.168.0.1/24")
