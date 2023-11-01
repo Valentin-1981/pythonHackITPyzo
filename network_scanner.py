05999230e79d8f32dd355070f00be5eada1210b7
Valentin-1981 <chibinyov@rambler.ru>
2023-11-01 13:00:32 +0300

    new commit

--- network_scanner.py
+++ network_scanner.py
@@ -1,4 +1,11 @@
 import scapy.all as scapy
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target", help="Target IP / IP range")
+    (options, arguments) = parser.parse_args()
+    return options
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
@@ -14,8 +21,17 @@ def scan(ip):
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     # print(answered_list.summary())
     print("IP\t\t\t\tMAC Address\n---------------------------------------------------------")
+    clients_list = []
     for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
         # print(element[1].show())
         print(element[1].psrc + "\t\t" + element[1].hwsrc)
 
+    print(clients_list)
+    return clients_list
+
+options = get_arguments()
+scan_result = scan(options.target)
+print(scan_result)
 scan("192.168.0.1/24")
